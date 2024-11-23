ebca67f5ef1bc4ce035ddb5f386ad9d76c17123b
fedotovalexander <fedotov.sanek@gmail.com>
2024-11-23 17:46:10 +0300

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Alexander Fedotov on 23.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
